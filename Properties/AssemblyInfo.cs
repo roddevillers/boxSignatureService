86f6827373d939999ee1e7db9350acbcadc38a66
roddevillers <rod.devillers@teoco.com>
2026-05-14 16:02:52 -0400

    Add AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("boxSignatureService")]
+[assembly: AssemblyDescription("ASP.NET Web API for Box.com Signature Requests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("boxSignatureService")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f3b5c5e5-5f5f-5f5f-5f5f-5f5f5f5f5f5f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
